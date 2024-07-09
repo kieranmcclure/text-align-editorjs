3eb361d6046cfecd17fa5df52fec0fbc329204c3
Kieran McClure <kieranmcclure97@gmail.com>
2024-07-09 15:49:50 +0100

    Building

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,35 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/main.js',
-		output: {
-			name: 'TextAlign',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
-		]
-	},
+  // browser-friendly UMD build
+  {
+    input: "src/main.js",
+    output: {
+      name: "TextAlign",
+      file: pkg.browser,
+      format: "umd",
+    },
+    plugins: [
+      resolve(), // so Rollup can find dependencies
+      commonjs(), // so Rollup can convert dependencies to ES modules
+      terser(), // to minify the UMD build
+    ],
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'src/main.js',
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build
+  {
+    input: "src/main.js",
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+    ],
+    plugins: [
+      resolve(), // so Rollup can find dependencies
+      commonjs(), // so Rollup can convert dependencies to ES modules
+      terser(), // to minify the CJS and ES builds
+    ],
+  },
 ];
